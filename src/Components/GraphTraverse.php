214b8d2f904cf1c8ba538b3915df7ed1f32d5722
Smoren <ofigate@gmail.com>
2022-08-26 00:56:00 +0300

    todo added

--- src/Components/GraphTraverse.php
+++ src/Components/GraphTraverse.php
@@ -22,6 +22,7 @@ class GraphTraverse
         // TODO вместо нескольких аргументов передавать TraverseContextInterface
         // TODO вместо Logger — TraverseHandler, методы onLoop и onVertex перенести туда
         // TODO внести флаг isLoop в контекст, избавиться от onLoop
+        // TODO onVertex должен иметь возможность принимать callback, чтобы не пришлось вводить getData() в context
         // TODO избавиться от рекурсии, принимая array<TraverseContextInterface> и выполняя while(count($contexts))
         $this->_runForward($startVertex, new Set(), 0);
     }
